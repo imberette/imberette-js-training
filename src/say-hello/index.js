92c218deeebba7c0d2397ec91e361691b0f2e6f1
Imber <anime.hater.alia@gmail.com>
2018-05-02 21:22:21 +0300

    solves say-hello

--- src/say-hello/index.js
+++ src/say-hello/index.js
@@ -1,1 +1,22 @@
-export default function () {}
+export default function (person) {
+  const hello = 'Hello! '
+  let result = hello
+
+  if (typeof person === 'string') {
+    result = result + person
+  }
+
+  if (typeof person === 'object' && person.name) {
+    result = result + person.name + ' '
+  }
+
+  if (typeof person === 'object' && person.lastname) {
+    result = result + person.lastname
+  }
+
+  if (!person) {
+    result = result + 'Stranger'
+  }
+
+  return result.trim()
+}
